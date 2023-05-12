52cea1aa3ebcf1f180f7caf80948f510bf451e42
bikrant malla <bikrant.malla@truenary.com>
2023-05-12 23:41:54 +0545

    feat: add a action in django admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["product_name", "product_image", "product_price", "product_offer", "product_status"]
+    list_filter = ["product_offer", "product_status"]
+    search_fields = ["product_name"]
+    actions = ["make_out_of_stock", "make_in_stock"]
 
-admin.site.register(Product)
+    def make_out_of_stock(self, request, queryset):
+        rows_updated = queryset.update(product_status="out_of_stock")
+        self.message_user(request, f"{rows_updated} product(s) marked as out of stock.")
+
+    make_out_of_stock.short_description = "Mark selected product(s) as out of stock"
+
+    def make_in_stock(self, request, queryset):
+        rows_updated = queryset.update(product_status="stock")
+        self.message_user(request, f"{rows_updated} product(s) marked as in stock.")
+
+    make_in_stock.short_description = "Mark selected product(s) as in stock"
+
+    def offer_label(self, obj):
+        return "On Offer" if obj.product_offer else "Not On Offer"
+
+    offer_label.short_description = "Offer Status"
+
+admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+
+class ProductStatusChoices(object):
+    stock = "stock"
+    out_of_stock = "out_of_stock"
+
+    choices = (
+        (stock, "Stock"),
+        (out_of_stock, "Out of Stock"),
+    )
+
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=30)
-    product_image = models.ImageField(upload_to = 'images')
+    product_image = models.ImageField(upload_to="images")
     product_price = models.IntegerField()
     product_offer = models.BooleanField(default=False)
+    product_status = models.CharField(
+        max_length=12,
+        choices=ProductStatusChoices.choices,
+        default=ProductStatusChoices.stock,
+    )
 
     def __str__(self):
         return self.product_name
